44b4559baf7d0c2474ff328b34478a27a0a73e2a
Wei Quan <limweiquan1@gmail.com>
2026-07-16 13:01:36 +0800

    nothing ever works

--- addons/extension_compiler/pizza_compiler_plugin.gd
+++ addons/extension_compiler/pizza_compiler_plugin.gd
@@ -5,16 +5,8 @@ func _enter_tree() -> void:
 	print("Project loaded. Recompiling pizza gdextension...")
 	var output = []
 
-# thank you grok...
-	var raw_path = "res://convenience files/build_cpp_debug.bat"
-	var global_path = ProjectSettings.globalize_path(raw_path)
-
-	OS.execute("cmd.exe", ["/c", global_path], output)
-	print("", output)
-# imagine checking... sorry grok...
-
-#	var exit_code = OS.execute("cmd.exe", ["/c", global_path], output)
-#	if exit_code == 0:
-#		print("pizza extension compiled successfully\n")
-#	else:
-#		printerr("Compilation failed: ", output)
+	var exit_code = OS.execute("scons", ["platform=windows", "target=template_debug"], output)
+	if exit_code == 0:
+		print("pizza extension compiled successfully\n")
+	else:
+		printerr("Compilation failed: ", output)
