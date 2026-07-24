fce7a6aedb3d7f91153242dfafe7c5d34fb09296
anan568 <an570owo@gmail.com>
2026-07-24 12:53:05 -0500

    sprite update and i broke surf_1 but thats o k

--- scripts/enemy/jump_box.gd
+++ scripts/enemy/jump_box.gd
@@ -3,6 +3,10 @@ extends Area2D
 class_name JumpBox
 
 var boost_force = 400
+@onready var animator = $AnimatedSprite2D
+
+func _ready() -> void:
+	animator.connect("animation_finished", Die)
 
 func Punched(pj: Node2D, force: float):
 	if pj.mounted == false:
@@ -12,4 +16,7 @@ func Punched(pj: Node2D, force: float):
 		pj.mounted = false
 		pj.velocity.y = -boost_force * force
 		
+	animator.play("crush")
+	
+func Die():
 	queue_free()
